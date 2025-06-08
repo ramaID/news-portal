72d814aae8d55c6ae93874bea3b5535f907df035
ramaID <rama@qisthi.dev>
2025-06-08 11:05:03 +0700

    reset post modules

--- modules/Post/Requests/Update.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace Modules\Post\Requests;
-
-class Update extends Store {}
--- modules/Post/resources/views/create.blade.php
+++
@@ -1,9 +0,0 @@
-<x-volt-app :title="__('laravolt::action.add') . ' Post'">
-    <x-volt-backlink url="{{ route('modules::post.index') }}"></x-backlink>
-
-    <x-volt-panel title="Tambah Post">
-        {!! form()->post(route('modules::post.store'))->horizontal()->multipart() !!}
-            @include('post::_form')
-        {!! form()->close() !!}
-    </x-volt-panel>
-</x-volt-app>
